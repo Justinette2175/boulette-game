a0d21972114b45d0cdf5033a4d3689ed9894caba
justinette2175 <jugagnepain75@gmail.com>
2020-05-20 23:20:50 -0400

    changed link

--- src/constants.tsx
+++ src/constants.tsx
@@ -3,4 +3,4 @@ export const SCORE_BOARD_WIDTH = 350;
 export const VIDEO_WIDTH = "700px";
 export const VIDEO_HEIGHT = "700px";
 
-export const BASE_URL = "localhost:3000";
+export const BASE_URL = "https://www.boulette.ca";
